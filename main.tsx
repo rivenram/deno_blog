81ee8d5222bb536126c7aeca4923a9928857c1b1
Riven Ramin Husin <s3822781@student.rmit.edu.au>
2023-03-10 20:19:08 +1100

    first commit

--- main.tsx
+++ main.tsx
@@ -3,14 +3,14 @@
 import blog, { ga, redirects, h } from "blog";
 
 blog({
-  title: "My Blog",
+  title: "Hello, world!",
   description: "This is my new blog.",
   // header: <header>Your custom header</header>,
   // section: <section>Your custom section</section>,
   // footer: <footer>Your custom footer</footer>,
   avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
   avatarClass: "rounded-full",
-  author: "An author",
+  author: "Riven Ramin Husinr",
 
   // middlewares: [
 
